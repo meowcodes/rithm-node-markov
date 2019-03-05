06fa61ebab5c8552d73741661133d1bd098a3325
meowcodes <meowcodes@gmail.com>
2019-03-05 11:16:32 -0800

    create makeChains()

--- markov.js
+++ markov.js
@@ -8,7 +8,7 @@ class MarkovMachine {
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
     this.words = words.filter(c => c !== "");
-    this.makeChains();
+    this.chain = this.makeChains();
   }
 
   /** set markov chains:
@@ -17,7 +17,35 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    let chain = {}
+
+    for (let i = 0; i < this.words.length; i++){
+
+      let thisWord = this.words[i].toLowerCase()
+      let nextWord = null
+
+      if(this.words[i+1]){
+        nextWord = this.words[i+1].toLowerCase()
+      }
+      
+      // drop '.' and change next word to null if end of sentence
+      if(thisWord.endsWith('.') || thisWord.endsWith(',')){
+        thisWord = thisWord.slice(0,-1)
+      }else {
+        if(nextWord.endsWith('.') || nextWord.endsWith(',')){
+          nextWord = nextWord.slice(0,-1)
+        }
+      }
+
+      // add to chain obj
+      if(!chain[thisWord]){
+        chain[thisWord] = [nextWord]
+      }else {
+        chain[thisWord].push(nextWord)
+      }
+    }
+
+    return chain;
   }
 
 
@@ -27,3 +55,6 @@ class MarkovMachine {
     // TODO
   }
 }
+
+
+module.exports = MarkovMachine
